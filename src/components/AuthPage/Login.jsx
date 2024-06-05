41d6de7abb62b91b25c6acb1f204046ae051a86d
Jeet Desai <jeetdesai2004@gmail.com>
2024-06-06 01:35:08 +0530

    added show password functionality in login page

--- src/components/AuthPage/Login.jsx
+++ src/components/AuthPage/Login.jsx
@@ -1,9 +1,11 @@
-import { Alert, AlertIcon, Button, Input } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import React, { useState } from "react";
 import useSignUpWithEmailPassword from "../../hooks/useSignUpWithEmailPassword";
 import useLoginWithEmailPassword from "../../hooks/useLoginWithEmailPassword";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 const Login = () => {
+  const [isHidden, Hide] = useState(true);
   const [input, setInput] = useState({
     email: "",
     password: "",
@@ -22,16 +24,30 @@ const Login = () => {
           setInput({ ...input, email: e.target.value });
         }}
       />
-      <Input
-        size={"sm"}
-        placeholder="Password"
-        type="password"
-        value={input.password}
-        fontSize={17}
-        onChange={(e) => {
-          setInput({ ...input, password: e.target.value });
-        }}
-      />
+      <InputGroup>
+        <Input
+          size={"sm"}
+          placeholder="Password"
+          type={isHidden ? "password" : "text"}
+          value={input.password}
+          fontSize={17}
+          onChange={(e) => {
+            setInput({ ...input, password: e.target.value });
+          }}
+        />
+
+        <InputRightElement h={"full"}>
+          <Button
+            variant={"ghost"}
+            size={"sm"}
+            onClick={() => {
+              Hide(!isHidden);
+            }}
+          >
+            {isHidden ?  <ViewOffIcon /> : <ViewIcon /> }
+          </Button>
+        </InputRightElement>
+      </InputGroup>
       {error && (
         <Alert status="error" fontSize={13} p={2} borderRadius={4}>
           <AlertIcon fontSize={12} />
